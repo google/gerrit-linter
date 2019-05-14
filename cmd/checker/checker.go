28003b5dd5d5f9629b7df64863aa2f21e7fc3105
Han-Wen Nienhuys <hanwen@google.com>
2019-05-14 09:28:53 +0200

    cmd/checker: handle unconfigured language

--- cmd/checker/checker.go
+++ cmd/checker/checker.go
@@ -141,7 +141,11 @@ func (c *gerritChecker) checkChange(changeID string, psID int, language string) 
 	}
 	req := gerritfmt.FormatRequest{}
 	for n, f := range ch.Files {
-		if !gerritfmt.Formatters[language].Regex.MatchString(n) {
+		cfg := gerritfmt.Formatters[language]
+		if cfg == nil {
+			return nil, fmt.Errorf("language %q not configured")
+		}
+		if !cfg.Regex.MatchString(n) {
 			continue
 		}
 
